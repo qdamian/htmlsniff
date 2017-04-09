d2592657b576f1c326a9e5a118fc7974f891a674
Damian Quiroga <qdamian@gmail.com>
2017-04-08 21:54:39 -0300

    Capturing request info in Bottle sniffer (WIP)

--- htmlvis/bottle_sniffer.py
+++ htmlvis/bottle_sniffer.py
@@ -1,3 +1,5 @@
+import time
+
 import bottle
 
 from .htmlvis import HTTPSniffer, Request, Response, Transaction
@@ -13,6 +15,7 @@ class BottleSniffer(HTTPSniffer):
     def __init__(self):
         super(BottleSniffer, self).__init__()
         self._transactions = []
+        self.start_time = time.time()
 
     @property
     def transactions(self):
@@ -21,19 +24,25 @@ class BottleSniffer(HTTPSniffer):
     def apply(self, callback, context):
         def decorator(*args, **kwargs):
             try:
+                request = self._gather_request_info(context)
                 return callback(*args, **kwargs)
             finally:
                 transaction = Transaction(
                     client_name='client name',
-                    request=Request(
-                        body=bottle.request.body.read(),
-                        elapsed=None,
-                        headers=None,
-                        method=None,
-                        url_path=None),
+                    request=request,
                     response=Response(
                         body=None, elapsed=None, headers=None, status=None),
                     server_name='server name')
                 self.transactions.append(transaction)
 
         return decorator
+
+    def _gather_request_info(self, context):
+        elapsed_time = time.time() - self.start_time
+        headers = {key: val for key, val in bottle.request.headers.items()}
+        return Request(
+            body=bottle.request.body.read(),
+            elapsed=elapsed_time,
+            headers=headers,
+            method=None,
+            url_path=None)
--- tests/test_bottle_sniffer.py
+++ tests/test_bottle_sniffer.py
@@ -1,37 +1,45 @@
+import time
+
 import bottle
+import pytest
 import webtest
 from htmlvis import BottleSniffer, HTTPSniffer, Transaction
-from pytest import raises
 
 app = bottle.Bottle()
 
 
-@app.route('/success')
+@pytest.fixture(autouse=True)
+def mock_time(mocker):
+    mocker.patch('time.time')
+    time.time.return_value = 10.123
+
+
+@app.post('/success')
+@app.get('/success')
 def success():
+    time.time.return_value = time.time() + 0.100
     return "To infinity and beyond!"
 
 
-@app.route('/error')
+@app.post('/error')
+@app.get('/error')
 def expliciterror():
+    time.time.return_value = time.time() + 0.200
     bottle.response.status = 400
     return 'Object already exists with that name'
 
 
-@app.route('/exception')
+@app.post('/exception')
+@app.get('/exception')
 def exceptionalerror():
     1 / 0
-
-
-@app.post('/name')
-def name():
-    return "Your name is %s" % bottle.request.body.read()
 
 
 def test_bottle_sniffer_is_an_http_sniffer():
     assert isinstance(BottleSniffer(), HTTPSniffer)
 
 
-class TestSucessfulTransactions():
+class TestSuccessfulTransactions():
     def test_sniffer_does_not_interfere(self):
         test_app = webtest.TestApp(app)
         app.install(BottleSniffer())
@@ -49,43 +57,96 @@ class TestSucessfulTransactions():
         test_app = webtest.TestApp(app)
         sniffer = BottleSniffer()
         app.install(sniffer)
-        test_app.post('/name', 'Michael')
+        test_app.post('/success', "Dreams + work")
         request = sniffer.transactions[0].request
-        assert request.body == b"Michael"
+        assert request.body == b"Dreams + work"
+
+    def test_the_elapsed_time_is_captured(self):
+        test_app = webtest.TestApp(app)
+        time.time.return_value = 2.3
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        time.time.return_value = 4.5
+        test_app.get('/success')
+        request = sniffer.transactions[0].request
+        assert abs(request.elapsed - 2.2) < 0.0001
+
+    def test_the_request_headers_are_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        test_app.get(
+            '/success',
+            headers={
+                'Content-Type': 'application/json',
+                'Some-Header': 'Some value'
+            })
+        request = sniffer.transactions[0].request
+        # not comparing dictionaries because WebTest adds extra headers
+        assert request.headers['Content-Type'] == 'application/json'
+        assert request.headers['Some-Header'] == 'Some value'
 
 
 class TestErrorTransactions():
     def test_sniffer_does_not_interfere(self):
         test_app = webtest.TestApp(app)
         app.install(BottleSniffer())
-        with raises(webtest.app.AppError) as excinfo:
+        with pytest.raises(webtest.app.AppError) as excinfo:
             test_app.get('/error')
         assert '400 Bad Request' in str(excinfo.value)
         assert 'Object already exists with that name' in str(excinfo.value)
 
-    def test_a_transaction_is_captured(self):
-        test_app = webtest.TestApp(app)
-        sniffer = BottleSniffer()
-        app.install(sniffer)
-        with raises(webtest.app.AppError):
-            test_app.get('/error')
-        assert len(sniffer.transactions) == 1
-        assert isinstance(sniffer.transactions[0], Transaction)
-
-
-class TestErrorTransactionsDueToExceptions():
-    def test_does_not_interfere(self):
+    def test_sniffer_does_not_interfere_with_error_due_to_exceptions(self):
         test_app = webtest.TestApp(app)
         app.install(BottleSniffer())
-        with raises(webtest.app.AppError) as excinfo:
+        with pytest.raises(webtest.app.AppError) as excinfo:
             test_app.get('/exception')
         assert '500 Internal Server Error' in str(excinfo.value)
 
-    def test_a_transaction_is_captured(self):
+    @pytest.mark.parametrize('url', ['/error', '/exception'])
+    def test_a_transaction_is_captured(self, url):
         test_app = webtest.TestApp(app)
         sniffer = BottleSniffer()
         app.install(sniffer)
-        with raises(webtest.app.AppError):
-            test_app.get('/exception')
+        with pytest.raises(webtest.app.AppError):
+            test_app.get(url)
         assert len(sniffer.transactions) == 1
         assert isinstance(sniffer.transactions[0], Transaction)
+
+    @pytest.mark.parametrize('url', ['/error', '/exception'])
+    def test_the_request_body_is_captured(self, url):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.post(url, '2 + 2 = 5')
+        request = sniffer.transactions[0].request
+        assert request.body == b"2 + 2 = 5"
+
+    def test_the_elapsed_time_is_captured(self):
+        test_app = webtest.TestApp(app)
+        time.time.return_value = 2.3
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        time.time.return_value = 4.5
+        with pytest.raises(webtest.app.AppError):
+            test_app.get('/error')
+        request = sniffer.transactions[0].request
+        assert abs(request.elapsed - 2.2) < 0.0001
+
+    @pytest.mark.parametrize('url', ['/error', '/exception'])
+    def test_the_request_headers_are_captured(self, url):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.get(
+                url,
+                headers={
+                    'Content-Type': 'application/json',
+                    'Some-Header': 'Some value'
+                })
+        request = sniffer.transactions[0].request
+        # not comparing dictionaries because WebTest adds extra headers
+        assert request.headers['Content-Type'] == 'application/json'
+        assert request.headers['Some-Header'] == 'Some value'
