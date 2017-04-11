15997663c442995145f3a3f6b800d1bc371bd4d5
Damian Quiroga <qdamian@gmail.com>
2017-04-10 23:51:47 -0300

    Capturing response info in Bottle sniffer

--- htmlvis/bottle_sniffer.py
+++ htmlvis/bottle_sniffer.py
@@ -1,3 +1,7 @@
+"""
+Bottle plugin to sniff HTTP transactions.
+See https://bottlepy.org/docs/dev/plugindev.html
+"""
 import time
 
 import bottle
@@ -7,10 +11,11 @@ from .htmlvis import HTTPSniffer, Request, Response, Transaction
 
 class BottleSniffer(HTTPSniffer):
     """
-    An Bottle plugin than can be installed in a Bottle application to capture
+    A Bottle plugin than can be installed in a Bottle application to capture
     HTTP transactions.
     See https://bottlepy.org/docs/dev/api.html#bottle.Bottle.install
     """
+    api = 2
 
     def __init__(self):
         super(BottleSniffer, self).__init__()
@@ -22,16 +27,29 @@ class BottleSniffer(HTTPSniffer):
         return self._transactions
 
     def apply(self, callback, context):
+        """
+        Transparently sniff a request, invoke the callback that processes it,
+        and finally sniff the returned response
+        """
+
         def decorator(*args, **kwargs):
+            response_status = '500 Internal Server Error'
+            handler_response = ''
             try:
                 request = self._gather_request_info(context)
-                return callback(*args, **kwargs)
+                handler_response = callback(*args, **kwargs)
+                response_status = bottle.response.status
+                return handler_response
+            except bottle.HTTPResponse as http_error:
+                response_status = http_error.status
+                raise
             finally:
+                response = self._gather_response_info(
+                    context, handler_response, response_status)
                 transaction = Transaction(
                     client_name='client name',
                     request=request,
-                    response=Response(
-                        body=None, elapsed=None, headers=None, status=None),
+                    response=response,
                     server_name='server name')
                 self.transactions.append(transaction)
 
@@ -44,5 +62,14 @@ class BottleSniffer(HTTPSniffer):
             body=bottle.request.body.read(),
             elapsed=elapsed_time,
             headers=headers,
-            method=context['method'],
+            method=context.method,
             url_path=bottle.request.path)
+
+    def _gather_response_info(self, context, handler_response, status):
+        elapsed_time = time.time() - self.start_time
+        headers = {key: val for key, val in bottle.response.headers.items()}
+        return Response(
+            body=handler_response,
+            elapsed=elapsed_time,
+            headers=headers,
+            status=status)
--- tests/test_bottle_sniffer.py
+++ tests/test_bottle_sniffer.py
@@ -1,3 +1,4 @@
+import json
 import time
 
 import bottle
@@ -7,31 +8,49 @@ from htmlvis import BottleSniffer, HTTPSniffer, Transaction
 
 app = bottle.Bottle()
 
+CAPTURE_START_TIME = 0.123
+SUCCESS_PROCESSING_TIME = 0.100
+ERROR_PROCESSING_TIME = 0.050
+
 
 @pytest.fixture(autouse=True)
 def mock_time(mocker):
     mocker.patch('time.time')
-    time.time.return_value = 10.123
+    time.time.return_value = CAPTURE_START_TIME
 
 
 @app.post('/success')
 @app.get('/success')
 def success():
-    time.time.return_value = time.time() + 0.100
+    time.time.return_value = time.time() + SUCCESS_PROCESSING_TIME
+    bottle.response.set_header('The-More', 'The-Merrier')
     return "To infinity and beyond!"
 
 
 @app.post('/error')
 @app.get('/error')
 def expliciterror():
-    time.time.return_value = time.time() + 0.200
+    time.time.return_value = time.time() + ERROR_PROCESSING_TIME
     bottle.response.status = 400
+    bottle.response.set_header('Today-Is', 'No-Way')
     return 'Object already exists with that name'
+
+
+@app.get('/http-response')
+def httpresponse():
+    body = json.dumps({'hello': 'world'})
+    raise bottle.HTTPResponse(status=300, body=body)
+
+
+@app.get('/http-error')
+def httperror():
+    raise bottle.HTTPError(status=404)
 
 
 @app.post('/exception')
 @app.get('/exception')
 def exceptionalerror():
+    time.time.return_value = time.time() + ERROR_PROCESSING_TIME
     1 / 0
 
 
@@ -39,19 +58,38 @@ def test_bottle_sniffer_is_an_http_sniffer():
     assert isinstance(BottleSniffer(), HTTPSniffer)
 
 
-class TestSuccessfulTransactions():
+def test_bottle_sniffer_implements_api_version_2():
+    # https://bottlepy.org/docs/dev/plugindev.html
+    assert BottleSniffer().api == 2
+
+
+def test_a_transaction_is_captured():
+    test_app = webtest.TestApp(app)
+    sniffer = BottleSniffer()
+    app.install(sniffer)
+    test_app.get('/success')
+    assert len(sniffer.transactions) == 1
+    assert isinstance(sniffer.transactions[0], Transaction)
+
+
+def test_multiple_requests_are_captured():
+    test_app = webtest.TestApp(app)
+    sniffer = BottleSniffer()
+    app.install(sniffer)
+    for _ in range(100):
+        test_app.get('/success').status
+        with pytest.raises(webtest.app.AppError):
+            test_app.get('/error').status
+        with pytest.raises(webtest.app.AppError):
+            test_app.get('/exception').status
+    assert len(sniffer.transactions) == 300
+
+
+class TestRequestDataCapturedInSuccessfulTransations():
     def test_sniffer_does_not_interfere(self):
         test_app = webtest.TestApp(app)
         app.install(BottleSniffer())
         assert test_app.get('/success').status == '200 OK'
-
-    def test_a_transaction_is_captured(self):
-        test_app = webtest.TestApp(app)
-        sniffer = BottleSniffer()
-        app.install(sniffer)
-        test_app.get('/success')
-        assert len(sniffer.transactions) == 1
-        assert isinstance(sniffer.transactions[0], Transaction)
 
     def test_the_request_body_is_captured(self):
         test_app = webtest.TestApp(app)
@@ -63,13 +101,14 @@ class TestSuccessfulTransactions():
 
     def test_the_elapsed_time_is_captured(self):
         test_app = webtest.TestApp(app)
-        time.time.return_value = 2.3
         sniffer = BottleSniffer()
         app.install(sniffer)
-        time.time.return_value = 4.5
+        REQUEST_TIME = 4.5
+        time.time.return_value = REQUEST_TIME
         test_app.get('/success')
         request = sniffer.transactions[0].request
-        assert abs(request.elapsed - 2.2) < 0.0001
+        expected_elapsed = REQUEST_TIME - CAPTURE_START_TIME
+        assert abs(request.elapsed - expected_elapsed) < 0.0001
 
     def test_the_request_headers_are_captured(self):
         test_app = webtest.TestApp(app)
@@ -105,7 +144,7 @@ class TestSuccessfulTransactions():
         assert request.url_path == '/success'
 
 
-class TestErrorTransactions():
+class TestRequestDataCapturedInFailedTransations():
     def test_sniffer_does_not_interfere(self):
         test_app = webtest.TestApp(app)
         app.install(BottleSniffer())
@@ -143,14 +182,15 @@ class TestErrorTransactions():
 
     def test_the_elapsed_time_is_captured(self):
         test_app = webtest.TestApp(app)
-        time.time.return_value = 2.3
         sniffer = BottleSniffer()
         app.install(sniffer)
-        time.time.return_value = 4.5
+        REQUEST_TIME = 4.5
+        time.time.return_value = REQUEST_TIME
         with pytest.raises(webtest.app.AppError):
             test_app.get('/error')
         request = sniffer.transactions[0].request
-        assert abs(request.elapsed - 2.2) < 0.0001
+        expected_elapsed = REQUEST_TIME - CAPTURE_START_TIME
+        assert abs(request.elapsed - expected_elapsed) < 0.0001
 
     @pytest.mark.parametrize('url', ['/error', '/exception'])
     def test_the_request_headers_are_captured(self, url):
@@ -191,3 +231,110 @@ class TestErrorTransactions():
             test_app.get(url)
         request = sniffer.transactions[0].request
         assert request.url_path == url
+
+
+class TestResponseDataCapturedInSuccessfulTransations():
+    def test_the_response_body_is_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        test_app.post('/success')
+        response = sniffer.transactions[0].response
+        assert response.body == "To infinity and beyond!"
+
+    def test_the_elapsed_time_is_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        REQUEST_TIME = 4.5
+        time.time.return_value = REQUEST_TIME
+        test_app.get('/success')
+        response = sniffer.transactions[0].response
+        expected_elapsed = (
+            REQUEST_TIME - CAPTURE_START_TIME + SUCCESS_PROCESSING_TIME)
+        assert abs(response.elapsed - expected_elapsed) < 0.0001
+
+    def test_the_response_headers_are_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        test_app.post('/success')
+        response = sniffer.transactions[0].response
+        assert response.headers == {'The-More': 'The-Merrier'}
+
+    def test_the_response_status_code_is_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        test_app.post('/success')
+        response = sniffer.transactions[0].response
+        assert response.status == '200 OK'
+
+
+class TestResponsetDataCapturedInFailedTransations():
+    def test_the_response_body_is_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.get('/error')
+        response = sniffer.transactions[0].response
+        assert 'Object already exists with that name' in response.body
+
+    @pytest.mark.parametrize('url', ['/error', '/exception'])
+    def test_the_elapsed_time_is_captured(self, url):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        REQUEST_TIME = 4.5
+        time.time.return_value = REQUEST_TIME
+        with pytest.raises(webtest.app.AppError):
+            test_app.get(url)
+        response = sniffer.transactions[0].response
+        expected_elapsed = (
+            REQUEST_TIME - CAPTURE_START_TIME + ERROR_PROCESSING_TIME)
+        assert abs(response.elapsed - expected_elapsed) < 0.0001
+
+    def test_the_response_headers_are_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.post('/error')
+        response = sniffer.transactions[0].response
+        assert response.headers == {'Today-Is': 'No-Way'}
+
+    def test_the_response_status_code_is_captured(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.post('/error')
+        response = sniffer.transactions[0].response
+        assert response.status == '400 Bad Request'
+
+    def test_status_code_is_captured_when_an_exception_is_raised(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.post('/exception')
+        response = sniffer.transactions[0].response
+        assert response.status == '500 Internal Server Error'
+
+    def test_status_code_is_captured_when_http_response_is_raised(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        test_app.get('/http-response')
+        response = sniffer.transactions[0].response
+        assert response.status == '300 Multiple Choices'
+
+    def test_status_code_is_captured_when_http_error_is_raised(self):
+        test_app = webtest.TestApp(app)
+        sniffer = BottleSniffer()
+        app.install(sniffer)
+        with pytest.raises(webtest.app.AppError):
+            test_app.get('/http-error')
+        response = sniffer.transactions[0].response
+        assert response.status == '404 Not Found'
